c95f103a91595135fed23eba53fe580b44d6c274
Natsu Tadama <sultan.ridita@gmail.com>
2023-02-14 16:37:55 +0700

    Create an automated setup script [BETA]

---
+++ setup.ps1
@@ -0,0 +1,164 @@
+﻿#!/usr/bin/env pwsh
+#Requires -Version 7
+
+# ==============================================================================
+# FUNCTIONS
+# ==============================================================================
+
+Function Show-SudoWarning {
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory = $True)]
+		[string]$Package
+	)
+
+	Write-Host @"
+`e[31mWARNING: This script will attempt to use sudo to install packages.`e[0m
+If you do not want to use sudo (or gsudo is not installed for Windows), please exit this script and install the following package manually:
+$package
+"@
+}
+
+Function Install-PackageFromDistro {
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory = $True)]
+		[string]$Package
+	)
+
+	$sudo = $True
+	$installParam = "install"
+
+	If ($IsLinux) {
+		# Check currently available package managers
+		If (Get-Command apx -ErrorAction SilentlyContinue) {
+			$PackageManager = "apx"
+			$sudo = $False
+		}
+		ElseIf (Get-Command apt -ErrorAction SilentlyContinue) {
+			$PackageManager = "apt"
+		}
+		ElseIf (Get-Command dnf -ErrorAction SilentlyContinue) {
+			$PackageManager = "dnf"
+		}
+		ElseIf (Get-Command yum -ErrorAction SilentlyContinue) {
+			$PackageManager = "yum"
+		}
+		ElseIf (Get-Command pacman -ErrorAction SilentlyContinue) {
+			$PackageManager = "pacman"
+			$installParam = "-Syu"
+		}
+		# Confirm if apk is available AND if it's Alpine
+		ElseIf (Get-Command apk -ErrorAction SilentlyContinue) {
+			If (Test-Path -Path "/etc/alpine-release") {
+				$PackageManager = "apk"
+				$installParam = "add"
+			}
+			Else {
+				Write-Error "apk: Not Alpine, unsupported OS" -ErrorAction Break
+			}
+		}
+		Else {
+			# Raise an error
+			Write-Error "Unsupported package manager" -ErrorAction Break
+		}
+	}
+	ElseIf ($IsMacOS) {
+		If (Test-Path -Path "/usr/local/bin/brew") {
+			$PackageManager = "brew"
+		}
+		Else {
+			Write-Error "Unsupported package manager" -ErrorAction Break
+		}
+	}
+	ElseIf ($IsWindows) {
+		If (Get-Command choco -ErrorAction SilentlyContinue) {
+			$PackageManager = "choco"
+		}
+		ElseIf (Get-Command winget -ErrorAction SilentlyContinue) {
+			$PackageManager = "winget"
+			$installParam = "install -e"
+		}
+		ElseIf (Get-Command scoop -ErrorAction SilentlyContinue) {
+			$PackageManager = "scoop"
+			$Sudo = $False
+		}
+		Else {
+			Write-Error "Unsupported package manager" -ErrorAction Break
+		}
+	}
+	Else {
+		Write-Error "Unsupported operating system" -ErrorAction Break
+	}
+
+	# Install the package
+	If ($sudo) {
+		Show-SudoWarning -Package $Package
+		Start-Process -FilePath "sudo" -ArgumentList "$PackageManager $installParam $Package" -Wait
+	}
+	Else {
+		Start-Process -FilePath "$PackageManager" -ArgumentList "$installParam $Package" -Wait
+	}
+}
+
+# ==============================================================================
+# PREQS
+# ==============================================================================
+
+# Check for binaries
+$binaries = @(
+	'python3',
+	'git'
+)
+
+$binaries | ForEach-Object {
+	If (-not (Get-Command $_ -ErrorAction SilentlyContinue)) {
+		# If $_ is python3, check for python
+		If ($_ -eq 'python3') {
+			If (-not (Get-Command 'python' -ErrorAction SilentlyContinue)) {
+				Write-Host "$_ is not installed"
+				Install-PackageFromDistro -Package 'python3'
+			}
+			Else {
+				Write-Host "$_ is already installed"
+			}
+		}
+		Else {
+			Write-Host "$_ is not installed"
+			Install-PackageFromDistro -Package $_
+		}
+	}
+	Else {
+		Write-Host "$_ is already installed"
+	}
+}
+
+# Check required PowerShell Modules
+$Modules = @(
+	'Set-PsEnv',
+	'PSGraphQL',
+	'PowerHTML'
+)
+
+$Modules | ForEach-Object {
+	If (-not (Get-Package -Name $_ -ErrorAction SilentlyContinue)) {
+		Write-Host "Installing module $_"
+		Install-Module -Name $_ -Force
+	}
+	Else {
+		Write-Host "Module $_ is already installed"
+	}
+}
+
+# Run Env Generator before running the rest of the script
+.\Modules\Generate-Env.ps1
+
+# ==============================================================================
+# RUN THE REST OF THE SCRIPT
+# ==============================================================================
+
+# Load the environment variables
+Set-PsEnv
+
+# Grab python, run pip
+Start-Process $PYTHON_PATH -ArgumentList '-m pip install -r requirements.txt' -Wait
